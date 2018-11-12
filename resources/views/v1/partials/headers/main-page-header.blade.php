1127deb95d2fa057a08ac408048147fda7ea7782
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-12 16:54:00 +0400

    header markup started

--- resources/views/v1/partials/headers/main-page-header.blade.php
+++ resources/views/v1/partials/headers/main-page-header.blade.php
@@ -1,26 +1,34 @@
 <div class="container-fluid px-0 main-page-header">
     <div class="container">
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand" href="{{route('/')}}"><img src="{{asset('img/logo/logo.svg')}}" alt="" class="logo-svg"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <div class="brand">
+                <a class="navbar-brand" href="{{route('/')}}"><img src="{{asset('img/logo/logo.svg')}}" alt="" class="logo-svg"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <span><i class="fas fa-phone"></i>8(800)888-99-88</span>
+                <ul class="navbar-nav mx-auto d-flex justify-content-around">
+                    <li class="nav-item align-self-center mx-4">
+                        <span><i class="fas fa-phone mr-1"></i>8(800)888-99-88</span>
                     </li>
-                    <li class="nav-item active">
-                        <span><i class="fas fa-phone"></i>8(800)888-99-88</span>
+                    <li class="nav-item align-self-center mx-4">
+                        <span><i class="fas fa-envelope mr-1"></i>client@brainor.ru</span>
                     </li>
-                    <li class="nav-item active">
-                        <span><i class="fas fa-phone"></i>8(800)888-99-88</span>
+                    <li class="nav-item mx-4">
+                        <span><i class="fab fa-2x fa-instagram"></i></span>
+                        <span><i class="fab fa-2x fa-vk"></i></span>
+                        <span><i class="fab fa-2x fa-facebook-f"></i></span>
+                        <span><i class="fab fa-2x fa-youtube"></i></span>
                     </li>
 
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <span class="input-group-text" id="basic-addon2">@example.com</span>
+                    </div>
                 </form>
             </div>
         </nav>
