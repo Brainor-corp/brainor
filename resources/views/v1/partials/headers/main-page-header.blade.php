ef2438d986607a224978ea64997dc898d91bcbb1
Sergey <89996249834@ya.ru>
2018-11-13 14:50:37 +0400

    save before changes

--- resources/views/v1/partials/headers/main-page-header.blade.php
+++ resources/views/v1/partials/headers/main-page-header.blade.php
@@ -1,35 +1,56 @@
+{{--<img src="{{asset('img/bg/flower.png')}}" alt="" class="bg-pics">--}}
+{{--<img src="{{asset('img/bg/')}}" alt="" class="bg-pics">--}}
+{{--<img src="{{asset('img/bg/')}}" alt="" class="bg-pics">--}}
+{{--<img src="{{asset('img/bg/')}}" alt="" class="bg-pics">--}}
 <div class="container-fluid px-0 main-page-header">
     <div class="container">
-        <nav class="navbar navbar-expand-lg">
-            <div class="brand">
+        <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="{{route('/')}}"><img src="{{asset('img/logo/logo.svg')}}" alt="" class="logo-svg"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-            </div>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto d-flex justify-content-around">
-                    <li class="nav-item align-self-center mx-4">
-                        <span><i class="fas fa-phone mr-1"></i>8(800)888-99-88</span>
+            <div class="collapse navbar-collapse flex-column" id="navbarSupportedContent">
+                <ul class="navbar-nav row w-100 navbar">
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center  ">
+                        <span class="text-nowrap"><i class="fas fa-phone mr-1"></i>8(800)888-99-88</span>
                     </li>
-                    <li class="nav-item align-self-center mx-4">
-                        <span><i class="fas fa-envelope mr-1"></i>client@brainor.ru</span>
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center  ">
+                        <span class="text-nowrap"><i class="fas fa-envelope mr-1"></i>client@brainor.ru</span>
                     </li>
-                    <li class="nav-item mx-4">
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center">
                         <span><i class="fab fa-2x fa-instagram"></i></span>
                         <span><i class="fab fa-2x fa-vk"></i></span>
                         <span><i class="fab fa-2x fa-facebook-f"></i></span>
                         <span><i class="fab fa-2x fa-youtube"></i></span>
                     </li>
-
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 p-0 align-self-center">
+                        <div class="searchboxwrapper">
+                            <input class="searchbox w-100" type="text" value="" name="search_input" placeholder="Поиск по сайту..." id="search_input">
+                            <i class="fas fa-1x fa-search searchsubmit"></i>
+                        </div>
+                    </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                        <span class="input-group-text" id="basic-addon2">@example.com</span>
-                    </div>
-                </form>
+                <ul class="navbar-nav row w-100 navbar">
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center  ">
+                        <span class="text-nowrap"><i class="fas fa-phone mr-1"></i>8(800)888-99-88</span>
+                    </li>
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center  ">
+                        <span class="text-nowrap"><i class="fas fa-envelope mr-1"></i>client@brainor.ru</span>
+                    </li>
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 align-self-center">
+                        <span><i class="fab fa-2x fa-instagram"></i></span>
+                        <span><i class="fab fa-2x fa-vk"></i></span>
+                        <span><i class="fab fa-2x fa-facebook-f"></i></span>
+                        <span><i class="fab fa-2x fa-youtube"></i></span>
+                    </li>
+                    <li class="nav-item col-lg-3 col-12 my-3 my-lg-0 p-0 align-self-center">
+                        <div class="searchboxwrapper">
+                            <input class="searchbox w-100" type="text" value="" name="search_input" placeholder="Поиск по сайту..." id="search_input">
+                            <i class="fas fa-1x fa-search searchsubmit"></i>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </nav>
     </div>
