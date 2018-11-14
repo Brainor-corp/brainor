ab74d3e900f371001c1b35c1d26de6bce93c912f
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-14 19:03:51 +0400

    header markup (at work)

--- resources/views/v1/foot/footer.blade.php
+++ resources/views/v1/foot/footer.blade.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Brainor-PC-000001
- * Date: 12.11.2018
- * Time: 14:23
- */
+<footer class="py-3">
+    <div class="d-flex justify-content-around">
+        <div class="d-flex flex-column">
+            Lorem.
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, tempora. <br>
+            Lorem ipsum dolor
+        </div>
+        <div class="d-flex flex-column">
+            Lorem.
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, totam? <br>
+            Lorem i
+        </div>
+        <div class="d-flex flex-column">
+            <b>Контакты</b>
+            <br>
+            <ul class="navbar-nav w-100 navbar">
+                <li class="nav-item">
+                    <span class="text-nowrap"><i class="fas fa-phone mr-1"></i>8(800)888-99-88</span>
+                </li>
+                <li class="nav-item">
+                    <span class="text-nowrap"><i class="fas fa-envelope mr-1"></i>client@brainor.ru</span>
+                </li>
+                <li class="nav-item d-flex flex-row">
+                    <span><i class="fab fa-2x fa-instagram"></i></span>
+                    <span><i class="fab fa-2x fa-vk"></i></span>
+                    <span><i class="fab fa-2x fa-facebook-f"></i></span>
+                    <span><i class="fab fa-2x fa-youtube"></i></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
