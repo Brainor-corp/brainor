2eef3a0030c36ab216698f79cac2dad7cf67986c
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-15 16:53:29 +0400

    header markup (at work)

--- resources/views/v1/pages/main-page/fragments/top-form.blade.php
+++ resources/views/v1/pages/main-page/fragments/top-form.blade.php
@@ -2,13 +2,13 @@
     <div class="row">
         <div class="col-12 col-lg-4">
             <h1 class="text-center text-lg-left text-uppercase">Комплексные решения по разработке <br>интеренет магазинов</h1>
-            <div class="top-form-container mt-5">
-                <div class="form-group text-center">
-                    <h3>Заполните форму и узнайте стоимость вашего проекта</h3>
+            <div class="top-form-container triangle-border mt-5">
+                <div class="form-group left-right-border p-5 text-center">
+                    <h4 class="mb-5">Заполните форму и узнайте стоимость вашего проекта</h4>
                     <input type="text" class="form-control my-2" placeholder="Ваше имя">
                     <input type="text" class="form-control my-2" placeholder="Ваш телефон">
                     <input type="text" class="form-control my-2" placeholder="Особенности проекта">
-                    <button class="green-btn btn">Просчитать проект</button>
+                    <button class="green-btn mt-5 btn">Просчитать проект</button>
                 </div>
             </div>
         </div>
