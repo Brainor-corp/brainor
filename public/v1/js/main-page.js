4d893233f891b120ab714e22854f9a0b8a592d11
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-19 16:40:26 +0400

    carriage text change

--- public/v1/js/main-page.js
+++ public/v1/js/main-page.js
@@ -1,48 +1,18 @@
-var i = 1;
-var deleting = false;
-var list = [
-    'интернет магазинов',
-    'всяких приколов',
-    'простаты',
-    'кеков и лелов',
-];
 $(document).ready(function () {
 
-    var cursor = $('#cursor');
+    var options = {
+        strings: [
+            'интернет магазинов',
+            'всяких приколов',
+            'простаты',
+            'кеков и лелов',
+        ],
+        typeSpeed: 40,
+        backSpeed: 40,
+        loop: true,
 
-    setInterval(function() {
-        if(!deleting){
-            cursor[0].style.display = (cursor[0].style.display == 'none' ? '' : 'none');
-        }
-    }, 500);
-    setInterval(function() {
-        if(!deleting){
-            rewrite();
-        }
-    }, 10000);
+    };
+
+    var typed = new Typed("#dynamic_changed_text", options);
 
 });
-
-function rewrite() {
-    var text = $('#dynamic_changed_text');
-    $('#cursor')[0].style.display = '';
-    deleting = true;
-    interval = setInterval(function() {
-        if(text.text().length > 0) {
-            text.text(text.text().substring(0, text.text().length - 1));
-        }
-        else{
-            clearInterval(interval);
-            var interval1 = setInterval(function() {
-                if (text.text().length !== list[i].length) {
-                    text.text(text.text() + list[i][text.text().length]);
-                }
-                else {
-                    clearInterval(interval1);
-                    (i === list.length -1) ? i=0 : i++;
-                    deleting = false;
-                }
-            }, 150);
-        }
-    }, 150);
-}
