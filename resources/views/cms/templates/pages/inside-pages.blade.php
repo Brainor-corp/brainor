e6d0193a1470e99a05b387137868227c0580a76e
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-19 10:23:11 +0400

    inside pages template

--- resources/views/cms/templates/pages/inside-pages.blade.php
+++ resources/views/cms/templates/pages/inside-pages.blade.php
@@ -5,11 +5,11 @@
  */
 ?>
 
-@extends('v1.layouts.inside-page-layout')
+@extends('v1.layouts.inside-pages-layout')
 
 @section('styles')
-    <link href="{{ asset("v1/css/pages/inside-pages/styles/inside-page.css") }}" rel="stylesheet">
-    <link href="{{ asset("v1/css/pages/inside-pages/media/inside-page-media.css") }}" rel="stylesheet">
+    <link href="{{ asset("v1/css/pages/inside-pages/styles/inside-pages.css") }}" rel="stylesheet">
+    <link href="{{ asset("v1/css/pages/inside-pages/media/inside-pages-media.css") }}" rel="stylesheet">
 @endsection
 
 @section('headScripts')
@@ -25,35 +25,15 @@
                 <div class="align-items-start">
                     <div class="text-justify">
                         <div class="text-justify">
-                            <div class="mb-5 fs-breadcrumbs decoration-links">
-                                @php
-                                    foreach ($page->ancestors as $ancestor)
-                                    {
-                                        $ancestors[] = $ancestor;
-                                    }
-                                    $ancestors[] = $page;
-                                @endphp
-                                <a href="/"><i class="fas fa-home"></i></a>
-                                @foreach($ancestors as $ancestor)
-                                    /<a href="{{ url($ancestor->url) }}">{{ $ancestor->title }}</a>
-                                @endforeach
-                            </div>
-                            <div class="my-5"><h1>{{ $page->title }}</h1></div>
-                            <div class="fs-text-post">
-                                {!! html_entity_decode($page->content) !!}
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3 col-md-4 col-12 main-content">
-                @include('v1.partials.sidebar.sidebar-pages')
             </div>
         </div>
     </div>
 @endsection
 
 @section('footScripts')
-    <script type="text/javascript" src="{{ asset('v1/js/sidebar.js') }}"></script>
-    {{--<script type="text/javascript" src="{{ asset('v1/plugins/multiselect/dist/js/selectize.js') }}"></script>--}}
 @endsection
