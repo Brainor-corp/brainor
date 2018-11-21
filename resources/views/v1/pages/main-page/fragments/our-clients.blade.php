cf661b67d0d8baa8063e2a55dd54de885643a4c1
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-21 11:05:33 +0400

    our clients are uploading from db and are links to cms

--- resources/views/v1/pages/main-page/fragments/our-clients.blade.php
+++ resources/views/v1/pages/main-page/fragments/our-clients.blade.php
@@ -8,66 +8,26 @@
     <div class="row p-0">
         @foreach($works as $work)
             <a href="{{$work->url}}" class="our-clients-img col-12 p-0 col-xl-4 w-100" style="background-image: url({{'../../../../../img/works/' . $work->slug . '.png'}})">
-                <div class="our-clients-img-blur p-3">
+                <div class="our-clients-img-blur"></div>
+                <div class="p-3 h-100">
                     <div class="our-clients-img-inner">
-                        <div class="row text-center">
-                            <div class="col-12 my-5"><img class="img-fluid" src="{{asset('img/works/logo-' . $work->slug . '.png')}}" alt=""></div>
-                            <div class="col-12 my-5"><b>{{$work->description}}</b></div>
+                        <div class="row text-center h-100">
+                            <div class="col-12 align-self-center"><img class="img-fluid work-logo" src="{{asset('img/works/logo-' . $work->slug . '.png')}}" alt=""></div>
+                            @if(isset($work->description))<div class="col-12 align-self-center green-hover-link"><h4>{{$work->description}}</h4></div>@endif
                         </div>
                     </div>
                 </div>
             </a>
         @endforeach
-        {{--<div class="our-clients-img col-12 p-0 col-xl-4 w-100">--}}
-            {{--<div class="our-clients-img-blur p-3">--}}
-                {{--<div class="our-clients-img-inner">--}}
-                    {{--<div class="row text-center">--}}
-                        {{--<div class="col-12 my-5"><img class="img-fluid" src="{{asset('img/praecedens.png')}}" alt=""></div>--}}
-                        {{--<div class="col-12 my-5">asdasd</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="our-clients-img col-12 p-0 col-xl-4 w-100">--}}
-            {{--<div class="our-clients-img-blur p-3">--}}
-                {{--<div class="our-clients-img-inner">--}}
-                    {{--<div class="row text-center">--}}
-                        {{--<div class="col-12 my-5"><img class="img-fluid" src="{{asset('img/praecedens.png')}}" alt=""></div>--}}
-                        {{--<div class="col-12 my-5">asdasd</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="our-clients-img col-12 p-0 col-xl-4 w-100">--}}
-            {{--<div class="our-clients-img-blur p-3">--}}
-                {{--<div class="our-clients-img-inner">--}}
-                    {{--<div class="row text-center">--}}
-                        {{--<div class="col-12 my-5"><img class="img-fluid" src="{{asset('img/praecedens.png')}}" alt=""></div>--}}
-                        {{--<div class="col-12 my-5">asdasd</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="our-clients-img col-12 p-0 col-xl-4 w-100">--}}
-            {{--<div class="our-clients-img-blur p-3">--}}
-                {{--<div class="our-clients-img-inner">--}}
-                    {{--<div class="row text-center">--}}
-                        {{--<div class="col-12 my-5"><img class="img-fluid" src="{{asset('img/praecedens.png')}}" alt=""></div>--}}
-                        {{--<div class="col-12 my-5">asdasd</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        <div class="our-clients-img col-12 p-0 col-xl-4 w-100">
+        <div class="our-clients-img col-12 p-0 col-xl-4 w-100" style="background-image: url({{'../../../../../img/works/us.png'}})">
             <div class="our-clients-img-blur p-3">
                 <div class="our-clients-img-inner">
-                    <div class="row text-center">
-                        <div class="col-12 my-5"><h1 class="text-uppercase">еще работы</h1></div>
-                        <div class="col-12 my-5"><a href="#" class="hvr-bounce-to-bottom btn">Смотреть все работы</a></div>
+                    <div class="row text-center h-100">
+                        <div class="col-12 align-self-center"><h1 class="text-uppercase">еще работы</h1></div>
+                        <div class="col-12 align-self-center"><a href="#" class="hvr-bounce-to-bottom btn">Смотреть все работы</a></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
