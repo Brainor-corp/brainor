40612d63dbdd97f1d4ee9067f9887d8f029ec199
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-11-28 14:40:10 +0400

    calculator markup

--- resources/views/v1/partials/finite-automate/1.blade.php
+++ resources/views/v1/partials/finite-automate/1.blade.php
@@ -1,14 +1,14 @@
-<div class="my-4 my-md-0 next text-center align-self-center" data-state="4">
+<div class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-state="4">
     <img src="{{asset('img/calculator/cart.png')}}" alt="">
     <br>
     <span>mysql</span>
 </div>
-<div class="my-4 my-md-0 next text-center align-self-center" data-state="5">
+<div class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-state="5">
     <img src="{{asset('img/calculator/site.png')}}" alt="">
     <br>
     <span>pgsql</span>
 </div>
-<div class="my-4 my-md-0 next text-center align-self-center" data-state="6">
+<div class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-state="6">
     <img src="{{asset('img/calculator/phone.png')}}" alt="">
     <br>
     <span>mongo</span>
