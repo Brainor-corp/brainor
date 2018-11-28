8c00b2ee165aeb808861462a3a46be5927b5729f
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2018-11-28 13:06:01 +0400

    На страницы партфолио добавлен баннер с заполнением брифа. Банер с заполнением брифа немного переработан. Убрана анимация предметов в шапке главной страницы.

--- resources/views/v1/partials/banners/start-now-banner.blade.php
+++ resources/views/v1/partials/banners/start-now-banner.blade.php
@@ -1,7 +1,13 @@
 <div class="container-fluid start-now-banner py-3">
-    <div class="green-block row align-items-center justify-content-around justify-content-md-center py-3 mx-0 text-center">
-        <span class="start-now-text text-uppercase col-12 col-xl-10">начните развивать свой бизнес прямо сейчас</span>
-        <a href="#" class="btn hvr-bounce-to-top align-text-bottom text-uppercase col-12 col-xl-auto">заполнить бриф</a>
+    <div class="green-block row align-items-center justify-content-around justify-content-md-center py-3 mx-0 px-lg-0 px-3 text-center">
+        <div class="start-now-text text-uppercase col-lg-auto col-12">начните развивать свой бизнес прямо сейчас</div>
+        <div class="btn hvr-bounce-to-top align-text-bottom text-uppercase col-lg-auto col-12 ml-lg-5 p-0">
+            <a href="#">
+                <div class="w-100 h-100 px-5 py-1">
+                    заполнить бриф
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
