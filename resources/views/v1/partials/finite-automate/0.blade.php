9f0e4d4bbd57543d1afcc57ec880ecb3618071f0
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-12-03 17:46:54 +0400

    поправлена верстка калькулятора

--- resources/views/v1/partials/finite-automate/0.blade.php
+++ resources/views/v1/partials/finite-automate/0.blade.php
@@ -2,17 +2,17 @@
     <div class="steps px-0 col-auto"><span id="step1" class="active past">Тип продукта</span></div>
 </div>
 <div class=" mt-5 row justify-content-around">
-    <div id="choice1" class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="0">
+    <div id="choice1" class="my-4 picture-next my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="0">
         <img src="{{asset('img/calculator/site.png')}}" alt="">
         <br>
         <span>Лэндинг</span>
     </div>
-    <div id="choice2" class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="10000">
+    <div id="choice2" class="my-4 picture-next my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="10000">
         <img src="{{asset('img/calculator/cart.png')}}" alt="">
         <br>
         <span>Магазин</span>
     </div>
-    <div id="choice3" class="my-4 my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="5000">
+    <div id="choice3" class="my-4 picture-next my-md-0 col-md-4 col-12 next text-center align-self-center" data-price="5000">
         <img src="{{asset('img/calculator/site.png')}}" alt="">
         <br>
         <span>Сайт компании</span>
