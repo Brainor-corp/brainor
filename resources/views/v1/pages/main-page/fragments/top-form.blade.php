a4c5397b488f49b653a53b0bd2da492ad66076b7
peresekinss <peresekinss@gmail.com>
2018-12-04 14:16:52 +0300

    Правки шапки медиаквери

--- resources/views/v1/pages/main-page/fragments/top-form.blade.php
+++ resources/views/v1/pages/main-page/fragments/top-form.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid top-form p-lg-5 p-3">
     <div class="row">
-        <div class="col-12 col-lg-5">
+        <div class="col-12 col-lg-7 col-xl-5">
             <h1 class="text-center text-lg-left text-uppercase">Комплексные решения по разработке <br> <span id="dynamic_changed_text"></span></h1>
             <div class="top-form-container triangle-border mt-5">
                 <div class="form-group left-right-border p-5 text-center" id="price-form">
