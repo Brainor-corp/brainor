1cb59adab59d83e66c87aa041e1ec6c3ba284da5
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2018-12-06 12:26:34 +0400

    трелло в работе

--- app/Http/Controllers/TrelloGenerator.php
+++ app/Http/Controllers/TrelloGenerator.php
@@ -3,33 +3,75 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class TrelloGenerator extends Controller
 {
-    public function generateTrelloReport(){
+    private $host = 'https://api.trello.com/1/';
+    private $key = '7b558af4d32e2b1e567482305e812033';
+    private $token = '604655c36dc2adf64b921b48fd9d1a4ef77a46daeb4929c8626a68e0cba4ff9d';
+    private $aliasesList = [
+        "start" => ["{s}"],
+        "pause" => ["{p}"],
+        "continue" => ["{c}"],
+        "end" => ["{e}"],
+    ];
+
+    public function generateTrelloReport(Request $request){
         $workers = User::whereNotNull('trello_id')->get();
+        $dateFrom = (new Carbon($request->dateFrom))->toIso8601ZuluString();
+        $since = (new Carbon($dateFrom))->subMonth()->toIso8601ZuluString();
+        $before = (new Carbon($request->dateTo))->toIso8601ZuluString();
+        $state = 0;
+
+        $groupedArray = [];
         foreach ($workers as $worker){
-            $actions = $this->getUsersActions($worker->trello_id, '2018-11-05T09:41:58.281Z', '2018-12-05T13:41:58.281Z');
-            dd($actions);
+            $actions = $this->getUsersActions($worker->trello_id, $since, $before);
+            foreach ($actions as $action) {
+                if(self::getCommentCommand($action['data']['text'])) {
+                    $groupedArray[$worker->name][$action['data']['board']['name']][$action['data']['card']['name']][] = ['text' => $action['data']['text'], 'date' => $action['date']];
+                }
+            }
         }
+
+        $resultArray = [];
+
+        foreach($groupedArray as $worker => $actions){
+            foreach ($actions as $board => $tasks){
+                foreach ($tasks as $task => $comments){
+                    foreach ($comments as $comment){
+                        $currentTaskName = null;
+                        $currentTimeSpend = null;
+                        $currentCompleteDate = null;
+                        switch($state){
+                            case 0:
+                                $state = 1;
+                                break;
+                            case 1:
+
+                                break;
+                            case 2:
+                                $state = 0;
+                                break;
+                            case 3:
+                                $state = 1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        dd($groupedArray);
     }
 
-    private function getUsersActions($userId, $since, $before){
-
-//_________defines___________//
-
-        $host = 'https://api.trello.com/1/';
-        $key = '7b558af4d32e2b1e567482305e812033';
-        $token = '604655c36dc2adf64b921b48fd9d1a4ef77a46daeb4929c8626a68e0cba4ff9d';
-        $getActionsByUserUrl = $host . 'members/' . $userId . '/actions?filter=commentCard&fields=id,data,date&key=' . $key . '&token=' . $token . '&since=' . $since . '&before=' . $before . '&memberCreator=false';
-
-//_________end defines_______//
+    private function getUsersActions($userId, $since = '', $before = ''){
+        $getActionsByUserPerMonthUrl = $this->host . 'members/' . $userId . '/actions?filter=commentCard&fields=id,data,date&key=' . $this->key . '&token=' . $this->token . '&since=' . $since . '&before=' . $before . '&memberCreator=false';
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $getActionsByUserUrl,
+            CURLOPT_URL => $getActionsByUserPerMonthUrl,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
@@ -38,8 +80,38 @@ class TrelloGenerator extends Controller
             CURLOPT_CUSTOMREQUEST => "GET",
         ));
 
-        $response = array_reverse(json_decode(curl_exec($curl), true));
+        $response = json_decode(curl_exec($curl), true);
         curl_close($curl);
         return $response;
     }
+
+    public function getCommentCommand($str) {
+        foreach ($this->aliasesList as $key => $aliases) {
+            if(in_array($str, $aliases)) {
+                return $key;
+            }
+        }
+        return 0;
+    }
 }
+
+//switch($state){
+//    case 0:
+//        $state = 1;
+//
+//        break;
+//    case 1:
+//        if($action['data']['text'] === '{s}'){
+//            $state = 2;
+//        }
+//        elseif($action['data']['text'] === '{c}'){
+//            $state = 3;
+//        }
+//        break;
+//    case 2:
+//        $state = 0;
+//        break;
+//    case 3:
+//        $state = 1;
+//        break;
+//}
